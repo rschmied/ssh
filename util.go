29a86a64d977525973a75ec65296033db72ba61a
Ralph Schmieder <rschmied@cisco.com>
2019-03-08 08:45:33 +0100

    allow rows/cols for PTYs to be zero
    Automation with stdin/out redirection will not have a tty, e.g. stty -a will report 0 for both rows and columns. This is fine, a PTY can be requested nevertheless... This is in line with what the OpenSSH server does.
    
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- util.go
+++ util.go
@@ -22,19 +22,7 @@ func parsePtyRequest(s []byte) (pty Pty, ok bool) {
 		return
 	}
 	width32, s, ok := parseUint32(s)
-	if width32 < 1 {
-		ok = false
-	}
-	if !ok {
-		return
-	}
 	height32, _, ok := parseUint32(s)
-	if height32 < 1 {
-		ok = false
-	}
-	if !ok {
-		return
-	}
 	pty = Pty{
 		Term: term,
 		Window: Window{
